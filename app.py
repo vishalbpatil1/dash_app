a07886dfb5408293f2f473cabb1c42eff32c823a
vishalbpatil1 <52704376+vishalbpatil1@users.noreply.github.com>
2020-06-08 22:45:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import dash_html_components as html
 import plotly.express as px
 df = px.data.iris() # iris is a pandas DataFrame
 fig = px.scatter(df, x="sepal_width", y="sepal_length")
-#fig.show()
 app = dash.Dash(__name__)
+#app = dash.Dash(__name__)
+server = app.server
 app.layout = html.Div([dcc.Graph(figure=fig)])
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
