098502a969202d8ae93710a62c2d38deb77db144
vishalbpatil1 <52704376+vishalbpatil1@users.noreply.github.com>
2020-06-09 10:24:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
-from flask import Flask
-df = px.data.iris() # iris is a pandas DataFrame
+url="https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/639388c2cbc2120a14dcf466e85730eb8be498bb/iris.csv"
+#names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+df=pd.read_csv(url)
 fig = px.scatter(df, x="sepal_width", y="sepal_length")
-server = flask.Flask('app')
-server.secret_key = os.environ.get('secret_key', 'secret')
-
-app = dash.Dash('app', server=server)
-
-app.scripts.config.serve_locally = False
-dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
+#fig.show()
+app = dash.Dash(__name__)
 app.layout = html.Div([dcc.Graph(figure=fig)])
 if __name__ == '__main__':
     app.run_server()
